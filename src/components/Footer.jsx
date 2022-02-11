3793b009554ebd1176fc51f74a20b81dfde2867d
Victor Giron <victor.giron@blackhill.dev>
2022-02-11 11:29:26 +0100

    footer with logo

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Footer (){
+    return  <footer>
+            <a href="https://www.alondracafe.com" target="_blank">Goutez notre café !</a>
+            <img src="/alondra_logo_solo.png"></img>
+            </footer> 
+}
+
+export default Footer
