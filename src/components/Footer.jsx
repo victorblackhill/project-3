119521045c2f6551b92c7c00cd5dfb91f06828e7
Victor Giron <victor.giron@blackhill.dev>
2022-02-11 12:40:31 +0100

    quick fixes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,11 @@ import React from "react"
 
 function Footer (){
     return  <>
+
+            <footer>
+            <a href="https://www.alondracafe.com" target="_blank">Goutez notre café !</a>
+            <img src="/alondra_logo_solo.png"></img>
+            </footer>
             <div className="footer-div">
                 <i className="fa fa-heart green"></i>
                 <i className="fa fa-coffee green"></i>
@@ -9,10 +14,6 @@ function Footer (){
                 <i className="fa fa-coffee green"></i>
                 <i className="fa fa-heart green"></i>
             </div>
-            <footer>
-            <a href="https://www.alondracafe.com" target="_blank">Goutez notre café !</a>
-            <img src="/alondra_logo_solo.png"></img>
-            </footer>
             </> 
 }
 
